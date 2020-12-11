248bf6afdc4d3df8caed1afe677ec2dfd99d3fcb
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-11 08:22:45 -0800

    Created Language class and set it to be a Data Table, named languages

---
+++ languages/src/main/java/com/practice/languages/models/Language.java
@@ -0,0 +1,103 @@
+package com.practice.languages.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "language")
+public class Language {
+	
+//	ATTRIBUTES
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Size(min = 5, max = 20)
+	private String name;
+	
+	@Size(min = 5, max = 20)
+	private String creator;
+	
+	@Min(1)
+	private Double currentVersion;
+	
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
+	
+//	Constructors
+	public Language() {
+	}
+	public Language(String name, String creator, Double currentVersion) {
+		this.name = name;
+		this.creator = creator;
+		this.currentVersion = currentVersion;
+	}
+
+//	PRESETS
+	@PrePersist
+	private void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	private void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+	
+//	METHODS
+
+	// Id
+	public Long getId() {
+		return id;
+	}
+
+	// Name
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Creator
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	// Current Version
+	public Double getCurrentVersion() {
+		return currentVersion;
+	}
+	public void setCurrentVersion(Double currentVersion) {
+		this.currentVersion = currentVersion;
+	}
+	
+	// DATES
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+}
